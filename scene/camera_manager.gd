8e1c6cd16ab5aff0c34c2346295e57a948980ea6
caiolflima <ilustralima@gmail.com>
2026-09-17 22:58:26 -0300

    Update em pronomes e Organização de Projeto

--- scene/camera_manager.gd
+++ scene/camera_manager.gd
@@ -3,7 +3,7 @@ const MOUSE_SENSITIVITY = 0.003
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		#global_position += event.relative
+		#global_position += event.relative - funciona de 2d pra 2d, de 2d(mouse) pra 3d usar o abaixo
 		global_position.y -= event.relative.x * MOUSE_SENSITIVITY
 		global_position.x -= event.relative.y * MOUSE_SENSITIVITY
 		print(event.relative)
