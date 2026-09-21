8207122856237361b2456127b73b6966fa5d2f50
caiolflima <ilustralima@gmail.com>
2026-09-21 16:26:48 -0300

    Correção Bug Resultado Dado

--- scene/dice.gd
+++ scene/dice.gd
@@ -4,8 +4,8 @@ extends RigidBody3D
 @export var rotation_speed_max: float
 @export var throw_speed_min: float
 @export var throw_speed_max: float
+@export var resultado: float
 @onready var faces: Node3D = $Faces
-@export var ligado: bool
 #@export var resultado: float
 
 var start_position: Transform3D
@@ -13,10 +13,9 @@ var result_emitted := false
 
 
 func _ready() -> void:
+	
 	start_position = transform
 	$".".hide()
-	ligado = false
-	
 	Dialogic.signal_event.connect(_on_signal)
 	_roll()
 
@@ -24,20 +23,23 @@ func _on_signal(signal_passed_in):
 	if signal_passed_in =="ligar_teste":
 		$".".show()
 		_roll()
+	elif signal_passed_in =="coletar_resultado": #metodo focado em coletar o resultado
+		_get_result()
+		Dialogic.VAR.resultado = resultado
 	
 #abaixo - quando o dado para de girar revela o valor:
 func _physics_process(_delta: float) -> void:
-	if sleeping and not result_emitted:
+	if sleeping and not result_emitted: #testando pegar o resultado apenas quando visivel
 		_get_result()
 	
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("roll") and ligado == true:
-		#visible = true #deixa o dado visivel na rolagem
-		_roll()
-		
+	if event.is_action_pressed("roll"):
+		Dialogic.start("nome_jogador_teste")
+
 func _get_result() -> void:
-	#resultado = float(_get_rolled_value())
-	Dialogic.VAR.set_variable("resultado", float(_get_rolled_value())) #devolve o resultado para o dialogicc
+	resultado = _get_rolled_value()
+	print(resultado)
+	#Dialogic.VAR.set_variable("resultado", resultado) #devolve o resultado para o dialogicc
 	result_emitted = true
 	
 func _reset_emitted() -> void:
@@ -59,6 +61,9 @@ func _roll() -> void:
 	var rotation_y = randf_range(rotation_speed_min, rotation_speed_max)
 	var rotation_z = randf_range(rotation_speed_min, rotation_speed_max)
 	apply_torque_impulse(Vector3(rotation_x, rotation_y, rotation_z))
+	
+	if sleeping and not result_emitted: #testando pegar o resultado apenas quando visivel
+		_get_result()
 	
 
 func _get_rolled_value() -> int:
