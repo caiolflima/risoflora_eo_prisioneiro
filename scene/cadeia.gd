8b30b2f1fa786fecd445c2f548d75afe29c8a70f
caiolflima <ilustralima@gmail.com>
2026-09-24 16:16:05 -0300

    Criação de Timeline Inicial e Cena da Cadeia

---
+++ scene/cadeia.gd
@@ -0,0 +1,21 @@
+extends Node3D
+@onready var ficha = preload("res://scene/ui_test.tscn") #endereco da cena da ficha
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Dialogic.start("capitulo1")
+	Dialogic.signal_event.connect(_on_signal)
+	AudioManager.play("Main") #toca a musica ambiente
+	$Alicate.hide()
+	$Galego.hide()
+	
+func _on_signal(signal_passed_in):
+	if signal_passed_in =="mostrar_ficha":
+		var ficha_tutorial = ficha.instantiate()
+		ficha_tutorial.global_position = Vector3(2,10,0)
+		add_child(ficha_tutorial)
+		print("Ficha")
+
+	if signal_passed_in =="acordar_presos":
+		$Alicate.show()
+		$Galego.show()
